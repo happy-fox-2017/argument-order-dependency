c02682c2669deb5541a33a069aed2dcaad521597
myyusuf <myyusuf1911@gmail.com>
2017-05-31 14:21:51 +0700

    Release 0: Use named arguments

--- house.js
+++ house.js
@@ -2,37 +2,37 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(options) {
+    this.address = options.address;
+    this.squareFeet = options.squareFeet;
+    this.numBedrooms = options.numBedrooms || 3;
+    this.numBaths = options.numBaths || 2;
+    this.cost = options.cost || 320000;
+    this.downPayment = options.downPayment || 0.20;
+    this.sold = options.sold || false;
+    this.shortSale = options.shortSale;
+    this.hasTenants = options.hasTenants || false;
   }
 
-  obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+  obscureAddress() {
+    this.address.replace(/.{10}$/g, '****');
   }
 
-  buy(money, good_credit) {
-    if (money >= down_payment && good_credit) {
-      this.sold = true
+  buy(money, goodCredit) {
+    if (money >= this.downPayment && goodCredit) {
+      this.sold = true;
     }
   }
 
-  down_payment() {
-    return cost * this.down_payment
+  downPayment() {
+    return this.cost * this.downPayment;
   }
 
-  to_s() {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
+  toS() {
+    return `${this.obscureAddress()} : ${this.squareFeet} sq. ft., ${this.numBedrooms} bed, ${this.numBaths} bath. ${this.cost}`;
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const cool = new House('address', 100, 2, 2, 12345, 12345, true, true);
 
-console.log(cool.to_s())
+console.log(cool.toS());
